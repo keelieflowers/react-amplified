5dfbf5abe3aab241d9395e85df6965b9d06a8a95
keelieflowers <keelie.flowers34@gmail.com>
2023-05-11 14:32:12 -0400

    remove unused amplify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 /* src/App.js */
 import React, { useEffect, useState } from 'react';
-import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import { API, graphqlOperation } from 'aws-amplify';
 import { createTodo } from './graphql/mutations';
 import { listTodos } from './graphql/queries';
-
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
 
 const initialState = { name: '', description: '' };
 
